9f8ee6aa885a23c6d91f67a65b754defef5c86a4
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2017-03-08 13:39:47 +1300

    editing readme file

---
+++ README.md
@@ -0,0 +1,11 @@
+# Bowling Kata
+
+## Learning Python
+
+1. Python
+1. Pytest
+1. Git repo
+1. Pip
+1. setup.py
+
+And so on
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     keywords = "example documentation tutorial",
     url = "http://packages.python.org/an_example_pypi_project",
     packages=['src'],
-    long_description=read('README'),
+    long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
